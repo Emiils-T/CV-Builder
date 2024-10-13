259d4ff05bb72907ed555675d351f102fe9de40a
emils-timma <153677791+Emiils-T@users.noreply.github.com>
2024-10-13 12:27:36 +0300

    Added CV Seeder

--- database/factories/CVFactory.php
+++ database/factories/CVFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use function MongoDB\BSON\toJSON;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CV>
