81009eaeaeac22aa05f97104f37c712e60697ba5
emils-timma <153677791+Emiils-T@users.noreply.github.com>
2024-10-13 20:30:49 +0300

    Added CV Controller index function

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CV;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CVController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+        $cv = CV::where('user_id', $user->id)->get();
+        return view('cv.index', compact('cv'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,10 @@
 <?php
 
+use App\Http\Controllers\CVController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [CVController::class])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
