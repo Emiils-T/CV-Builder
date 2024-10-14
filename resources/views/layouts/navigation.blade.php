6dd02f10eb09b3bfef5cb0dcd9342c71432a11af
emils-timma <153677791+Emiils-T@users.noreply.github.com>
2024-10-14 13:19:37 +0300

    Added guest nav option

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -80,6 +80,9 @@
                     </x-responsive-nav-link>
                 </form>
 
+
+
+
             @else
                 <div class="pt-2 pb-3 space-y-1">
                     <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
