24fdc4485f94ca8198aa802810dbe56d45ac7742
emils-timma <153677791+Emiils-T@users.noreply.github.com>
2024-10-16 20:45:13 +0300

    Small index view add

--- resources/views/cv/index.blade.php
+++ resources/views/cv/index.blade.php
@@ -2,4 +2,7 @@
     @if($cv->empty())
         <div class="content-center"> Appears that you have no cv's. </div>
     @endif
+    @foreach($cv as $resume)
+        <div>{{$resume->title}}</div>
+    @endforeach
 </x-app-layout>
