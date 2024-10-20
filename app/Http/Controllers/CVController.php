641607214fd2a34913538fa46174ce9220451881
emils-timma <153677791+Emiils-T@users.noreply.github.com>
2024-10-20 21:13:08 +0300

    added delete function to cv class

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -14,44 +14,68 @@ class CVController extends Controller
         $cv = CV::where('user_id', $user->id)->get();
         return view('cv.index', compact('cv'));
     }
-    public function create()
-    {
-        return view('cv.create');
-    }
-    public function store(Request $request)
-    {
-        /*dd($request->all());*/
-        $validated = $request->validate([
-            'firstName' => 'required|max:255',
-            'surname' => 'required|max:255',
-            'email' =>  'required|max:255',
-            'phoneNumber' =>  'required|max:255',
-            'dateOfBirth' =>  'required|date',
-            'address' =>  'max:255',
-            'experience' =>  'string',
-            'education' =>  'required|string',
-            'languages' => 'required|string',
-            'skills' => 'string',
-        ]);
-    }
-
+    //TODO: add cv.show blade view
+    //TODO: add cv.edit blade view
     public function show($id)
     {
 
     }
+
+    public function create()
+    {
+        return view('cv.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'firstName' => 'required|max:255',
+            'surname' => 'required|max:255',
+            'email' => 'required|max:255',
+            'phoneNumber' => 'required|max:255',
+            'dateOfBirth' => 'required|date',
+            'address' => 'max:255',
+            'experience' => 'string',
+            'education' => 'required|string',
+            'languages' => 'required|string',
+            'skills' => 'string',
+        ]);
+
+        $user = Auth::user();
+        $cv = $user->cv()->create($validated);
+        return redirect('cv.index',compact('cv'));
+    }
+
     public function edit($id)
     {
-
+        return view('cv.edit');
     }
+
     public function update(Request $request, $id)
     {
 
+        $validated = $request->validate([
+            'firstName' => 'required|max:255',
+            'surname' => 'required|max:255',
+            'email' => 'required|max:255',
+            'phoneNumber' => 'required|max:255',
+            'dateOfBirth' => 'required|date',
+            'address' => 'max:255',
+            'experience' => 'string',
+            'education' => 'required|string',
+            'languages' => 'required|string',
+            'skills' => 'string',
+        ]);
+        $cv = CV::where('id', $id);
+
+        $cv->update($validated);
+        return redirect('cv.show', compact('cv'));
     }
+
     public function destroy($id)
     {
         $cv = CV::where('id', $id)->first();
         $cv->delete();
-        //TODO: add validation
     }
 
 
